65f7d3747fa2f8c468c7eb10937da724604203ba
Blackjack Kent <blackjacksoftwaredev@gmail.com>
2017-11-28 13:33:44 -0600

    Updating character table

--- src/display/views/characters/components/_columns.js
+++ src/display/views/characters/components/_columns.js
@@ -4,7 +4,7 @@ export default [
 	{
 		Header: 'Character Name',
 		accessor: 'characterName',
-		Cell: row => <span className={row.original.isOnHiatus ? 'text-muted' : ''}>{row.value ? row.value : 'Unnamed Character'} {row.original.isOnHiatus ? '(On Hiatus)' : ''}</span>
+		Cell: row => <span className={row.original.isOnHiatus ? 'text-muted' : ''}>{row.value ? row.value : 'Unnamed Character'}</span>
 	}, {
 		Header: 'URL Identifier',
 		accessor: 'urlIdentifier',
@@ -12,6 +12,10 @@ export default [
 	}, {
 		Header: 'Platform',
 		accessor: 'platform.platformName'
+	}, {
+		Header: 'Status',
+		accessor: 'isOnHiatus',
+		Cell: row => <span className={row.original.isOnHiatus ? 'text-muted' : ''}> {row.value ? 'On Hiatus' : 'Active'} </span>
 	}, {
 		expander: true,
 		width: 65,
