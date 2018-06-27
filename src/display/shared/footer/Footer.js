45fda4bfa37d973840789803b7fbab9b11ada9b9
Blackjack Kent <blackjacksoftwaredev@gmail.com>
2018-06-26 20:27:23 -0500

    Footer unit tests

--- src/display/shared/footer/Footer.js
+++ src/display/shared/footer/Footer.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-const currentYear = (new Date()).getFullYear();
-
-const Footer = () => (
-	<footer className="app-footer">
-		<a href="http://www.rpthreadtracker.com">RPThreadTracker</a> &copy; {currentYear} <a href="http://blackjack-software.com">Blackjack Software</a>
-		<span className="float-right">Powered by <a href="http://coreui.io">CoreUI</a></span>
-	</footer>
-);
+const Footer = () => {
+	const currentYear = (new Date()).getFullYear();
+	return (
+		<footer className="app-footer">
+			<a href="http://www.rpthreadtracker.com">RPThreadTracker</a> &copy; {currentYear} <a href="http://blackjack-software.com">Blackjack Software</a>
+			<span className="float-right">Powered by <a href="http://coreui.io">CoreUI</a></span>
+		</footer>
+	);
+};
 
 export default Footer;
