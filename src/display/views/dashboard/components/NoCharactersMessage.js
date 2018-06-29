441b771ffbd5a55d84db6230d0799d5b124959a8
Blackjack Kent <blackjacksoftwaredev@gmail.com>
2018-06-29 15:46:01 -0500

    Front-end tweaks

--- src/display/views/dashboard/components/NoCharactersMessage.js
+++ src/display/views/dashboard/components/NoCharactersMessage.js
@@ -3,7 +3,8 @@ import React from 'react';
 const NoCharactersMessage = () => (
 	<div className="dashboard-card-center-message">
 		You have not yet added any characters to your account.<br />
-		Click &quot;Add Character&quot; to add one.
+		Click &quot;Add Character&quot; in the header menu{' '}
+		to add one.
 	</div>
 );
 
--- src/display/views/dashboard/components/NoThreadsMessage.js
+++ src/display/views/dashboard/components/NoThreadsMessage.js
@@ -3,7 +3,8 @@ import React from 'react';
 const NoThreadsMessage = () => (
 	<div className="dashboard-card-center-message">
 		You do not currently have any threads tracked.<br />
-		Click &quot;Track New Thread&quot; to add a thread to your account.
+		Click &quot;Track New Thread&quot; in the header menu{' '}
+		to add a thread to your account.
 	</div>
 );
 
