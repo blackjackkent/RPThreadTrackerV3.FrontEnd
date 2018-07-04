778b301d1509a58d86fda2fa2fe62c135ab33f9b
Blackjack Kent <blackjacksoftwaredev@gmail.com>
2018-07-03 22:23:00 -0500

    Sidebar tests

--- src/display/shared/sidebar/Sidebar.js
+++ src/display/shared/sidebar/Sidebar.js
@@ -5,40 +5,14 @@ import classNames from 'classnames';
 import nav from './_nav';
 
 class Sidebar extends Component {
-	constructor(props) {
-		super(props);
-		this.handleClick = this.handleClick.bind(this);
-		this.activeRoute = this.activeRoute.bind(this);
-	}
-
-	handleClick(e) {
-		e.preventDefault();
-		e.target.parentElement.classList.toggle('open');
-	}
-
-	activeRoute(routeName, props) {
-		return props.location.pathname.indexOf(routeName) > -1 ? 'nav-item nav-dropdown open' : 'nav-item nav-dropdown';
-	}
-
 	render() {
-		// simple wrapper for nav-title item
-		const wrapper = (item) => {
-			if (!item.wrapper) {
-				return item.name;
-			}
-			return (React.createElement(item.wrapper.element, item.wrapper.attributes, item.name));
-		};
-
-		// nav list section title
+		const wrapper = item => (
+			React.createElement(item.wrapper.element, item.wrapper.attributes, item.name)
+		);
 		const title = (titleData, key) => {
 			const classes = classNames('nav-title', titleData.class);
 			return (<li key={key} className={classes}>{wrapper(titleData)} </li>);
 		};
-
-		// nav list divider
-		const divider = (dividerData, key) => (<li key={key} className="divider" />);
-
-		// nav item with nav link
 		const navItem = (item, key) => {
 			const classes = classNames('nav-link', item.class);
 			return (
@@ -49,14 +23,9 @@ class Sidebar extends Component {
 				</NavItem>
 			);
 		};
-
-		// nav link
 		const navLink = (item, idx) => {
 			if (item.title) {
 				return title(item, idx);
-			}
-			if (item.divider) {
-				return divider(item, idx);
 			}
 			return navItem(item, idx);
 		};
