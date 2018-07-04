fae40d994f39f350b8a586594a69c49583c05e68
Blackjack Kent <blackjacksoftwaredev@gmail.com>
2018-07-03 22:56:16 -0500

    Loading indicator tests

---
+++ src/display/shared/__tests__/LoadingIndicator.spec.js
@@ -0,0 +1,26 @@
+// #region imports
+import React from 'react';
+import { shallow } from 'enzyme';
+import { getSpecWrapper } from '../../../../config/tests/helpers.unit';
+import LoadingIndicator from '../LoadingIndicator';
+// #endregion imports
+
+const createTestProps = propOverrides => ({
+	...propOverrides
+});
+describe('rendering', () => {
+	describe('snapshots', () => {
+		it('should render valid snapshot', () => {
+			const props = createTestProps();
+			const jsx = (<LoadingIndicator {...props} />);
+			const element = shallow(jsx);
+			expect(element).toMatchSnapshot();
+		});
+		it('should render valid snapshot with passed props', () => {
+			const props = createTestProps({ style: { width: 60, height: 70, lineHeight: 10 }, className: 'my-test-class' });
+			const jsx = (<LoadingIndicator {...props} />);
+			const element = shallow(jsx);
+			expect(element).toMatchSnapshot();
+		});
+	});
+});
