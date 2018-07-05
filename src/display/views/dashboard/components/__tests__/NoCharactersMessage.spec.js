c7c037a85f59c95e5b3b1456bfa16014005a6c17
Blackjack Kent <blackjacksoftwaredev@gmail.com>
2018-07-05 16:21:18 -0500

    Fallback message tests

---
+++ src/display/views/dashboard/components/__tests__/NoCharactersMessage.spec.js
@@ -0,0 +1,20 @@
+// #region imports
+import React from 'react';
+import { shallow } from 'enzyme';
+import NoCharactersMessage from '../NoCharactersMessage';
+// #endregion imports
+
+const createTestProps = propOverrides => ({
+	...propOverrides
+});
+
+describe('rendering', () => {
+	describe('snapshots', () => {
+		it('should render valid snapshot', () => {
+			const props = createTestProps();
+			const jsx = (<NoCharactersMessage {...props} />);
+			const element = shallow(jsx);
+			expect(element).toMatchSnapshot();
+		});
+	});
+});
---
+++ src/display/views/dashboard/components/__tests__/NoThreadsMessage.spec.js
@@ -0,0 +1,20 @@
+// #region imports
+import React from 'react';
+import { shallow } from 'enzyme';
+import NoThreadsMessage from '../NoThreadsMessage';
+// #endregion imports
+
+const createTestProps = propOverrides => ({
+	...propOverrides
+});
+
+describe('rendering', () => {
+	describe('snapshots', () => {
+		it('should render valid snapshot', () => {
+			const props = createTestProps();
+			const jsx = (<NoThreadsMessage {...props} />);
+			const element = shallow(jsx);
+			expect(element).toMatchSnapshot();
+		});
+	});
+});
