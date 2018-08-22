20c2c36dc2095f25b9ecdaa3ac16faa9c859afb8
Blackjack Kent <blackjacksoftwaredev@gmail.com>
2018-08-22 17:05:03 -0500

    Cleaning up webpack configs

---
+++ webpack.dev.js
@@ -0,0 +1,26 @@
+const merge = require('webpack-merge');
+const webpack = require('webpack');
+const path = require('path');
+const common = require('./webpack.common.js');
+const config = require('./config/config.dev.json');
+
+const BUILD_DIR = path.resolve(__dirname, 'build');
+
+module.exports = merge(common, {
+	mode: 'development',
+	devtool: 'cheap-module-eval-source-map',
+	devServer: {
+		contentBase: BUILD_DIR,
+		compress: true,
+		hot: true,
+		open: true,
+		overlay: true
+	},
+	plugins: [
+		new webpack.DefinePlugin({
+			API_BASE_URL: JSON.stringify(config.API_BASE_URL),
+			TUMBLR_CLIENT_BASE_URL: JSON.stringify(config.TUMBLR_CLIENT_BASE_URL)
+		}),
+		new webpack.HotModuleReplacementPlugin(),
+	]
+});
---
+++ webpack.prod.js
@@ -0,0 +1,23 @@
+const merge = require('webpack-merge');
+const webpack = require('webpack');
+const CompressionPlugin = require('compression-webpack-plugin');
+const common = require('./webpack.common.js');
+const config = require('./config/config.prod.json');
+
+module.exports = merge(common, {
+	mode: 'production',
+	devtool: 'source-map',
+	plugins: [
+		new webpack.DefinePlugin({
+			API_BASE_URL: JSON.stringify(config.API_BASE_URL),
+			TUMBLR_CLIENT_BASE_URL: JSON.stringify(config.TUMBLR_CLIENT_BASE_URL)
+		}),
+		new CompressionPlugin()
+	],
+	optimization: {
+		splitChunks: {
+			chunks: 'all'
+		},
+		minimize: true
+	}
+});
