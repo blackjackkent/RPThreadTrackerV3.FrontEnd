9dd718381cdb6c0956bf3a4371c0d8476e267800
Blackjack Kent <blackjacksoftwaredev@gmail.com>
2018-09-26 14:19:07 -0500

    Some selector tests

--- src/infrastructure/api/__tests__/isValidSlug.spec.js
+++ src/infrastructure/api/__tests__/isValidSlug.spec.js
@@ -7,12 +7,20 @@ jest.mock('axios', () => ({
 }));
 
 describe('behavior', () => {
-	it('should return rejected promise when GET request fails', async () => {
+	it('should return rejected promise when GET request fails and view ID provided', async () => {
 		axios.get.mockImplementationOnce(() => Promise.reject());
 		return expect(isValidSlug('test-slug', '12345')).rejects.toBe(undefined);
 	});
-	it('should return resolved promise when GET request succeeds', async () => {
+	it('should return rejected promise when GET request fails and view ID not provided', async () => {
+		axios.get.mockImplementationOnce(() => Promise.reject());
+		return expect(isValidSlug('test-slug')).rejects.toBe(undefined);
+	});
+	it('should return resolved promise when GET request succeeds and view ID provided', async () => {
 		axios.get.mockImplementationOnce(() => Promise.resolve());
 		return expect(isValidSlug('test-slug', '12345')).resolves.toBe(undefined);
+	});
+	it('should return resolved promise when GET request succeeds and view ID not provided', async () => {
+		axios.get.mockImplementationOnce(() => Promise.resolve());
+		return expect(isValidSlug('test-slug')).resolves.toBe(undefined);
 	});
 });
---
+++ src/infrastructure/selectors/common/__tests__/filterThreadsByPublicViewFilter.spec.js
@@ -0,0 +1,70 @@
+import filterThreadsByPublicViewFilter from '../filterThreadsByPublicViewFilter';
+
+const getThreads = () => [
+	{
+		thread: { threadId: 1, isArchived: true },
+		status: { isQueued: true, isCallingCharactersTurn: true }
+	},
+	{
+		thread: { threadId: 2, isArchived: true },
+		status: { isQueued: true, isCallingCharactersTurn: false }
+	},
+	{
+		thread: { threadId: 3, isArchived: true },
+		status: { isQueued: false, isCallingCharactersTurn: true }
+	},
+	{
+		thread: { threadId: 4, isArchived: true },
+		status: { isQueued: false, isCallingCharactersTurn: false }
+	},
+	{
+		thread: { threadId: 5, isArchived: true }
+	},
+	{
+		thread: { threadId: 6, isArchived: false },
+		status: { isQueued: true, isCallingCharactersTurn: true }
+	},
+	{
+		thread: { threadId: 7, isArchived: false },
+		status: { isQueued: true, isCallingCharactersTurn: false }
+	},
+	{
+		thread: { threadId: 8, isArchived: false },
+		status: { isQueued: false, isCallingCharactersTurn: true }
+	},
+	{
+		thread: { threadId: 9, isArchived: false },
+		status: { isQueued: false, isCallingCharactersTurn: false }
+	},
+	{
+		thread: { threadId: 10, isArchived: false }
+	}
+];
+
+describe('behavior', () => {
+	it('should filter archived threads', () => {
+		const threads = getThreads();
+		const result = filterThreadsByPublicViewFilter(threads, 'Archived');
+		expect(result).toHaveLength(5);
+	});
+	it('should filter queued threads', () => {
+		const threads = getThreads();
+		const result = filterThreadsByPublicViewFilter(threads, 'Queued');
+		expect(result).toHaveLength(2);
+	});
+	it('should filter partner threads', () => {
+		const threads = getThreads();
+		const result = filterThreadsByPublicViewFilter(threads, "Partner's Turn");
+		expect(result).toHaveLength(1);
+	});
+	it('should filter my threads', () => {
+		const threads = getThreads();
+		const result = filterThreadsByPublicViewFilter(threads, 'My Turn');
+		expect(result).toHaveLength(3);
+	});
+	it('should return all threads with invalid filter', () => {
+		const threads = getThreads();
+		const result = filterThreadsByPublicViewFilter(threads, 'Test');
+		expect(result).toHaveLength(10);
+	});
+});
