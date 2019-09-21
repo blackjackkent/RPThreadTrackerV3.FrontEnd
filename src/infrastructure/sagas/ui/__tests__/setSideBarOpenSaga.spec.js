2ebff50599decd6e582771b21634a0c24d6e327f
Ash <29510990+wharrrgarbl@users.noreply.github.com>
2019-09-21 14:17:44 -0400

    Fix paths in new files from develop

--- src/infrastructure/sagas/ui/__tests__/setSideBarOpenSaga.spec.js
+++ src/infrastructure/sagas/ui/__tests__/setSideBarOpenSaga.spec.js
@@ -1,6 +1,6 @@
 import setSideBarOpenSaga from '../setSideBarOpenSaga';
 import * as actions from '../../../actions';
-import { SagaTestWrapper } from '../../../../../config/tests/helpers.unit';
+import { SagaTestWrapper } from '~/testhelpers/helpers.unit';
 
 jest.mock('../../../cache', () => ({
 	set: jest.fn()
