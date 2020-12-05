add16ca3a833f40a3c5ffb530a1d7ba5c3f3d2b8
Niaz <mnuhaque@myseneca.ca>
2020-12-05 18:28:54 -0500

    (Update) PR#147 - Issue #144 | Fixed after reviews

--- src/display/views/help/components/ContactFormPane.js
+++ src/display/views/help/components/ContactFormPane.js
@@ -1,41 +1,14 @@
 import React from 'react';
 import { TabPane, CardHeader, CardBody } from 'reactstrap';
-import { AvForm } from 'availity-reactstrap-validation';
-// import PropTypes from 'prop-types';
 import Card from '../../../shared/styled/Card';
 
-// const propTypes = {
-// 	// submitContactForm: PropTypes.func.isRequired
-// };
 class ContactFormPane extends React.Component {
 	constructor() {
 		super();
-		this.handleInputChange = this.handleInputChange.bind(this);
-		this.submitForm = this.submitForm.bind(this);
-		this.state = {
-			formData: {}
-		};
-	}
-
-	handleInputChange(event) {
-		const { target } = event;
-		const { name, value } = target;
-		this.setState((prevState) => ({
-			formData: Object.assign({}, prevState.formData, {
-				[name]: value
-			})
-		}));
-	}
-
-	submitForm() {
-		window.location.assign(
-			'https://github.com/blackjackkent/RPThreadTrackerV3.FrontEnd/issues/new'
-		);
+		this.state = {};
 	}
 
 	render() {
-		// const { submitContactForm } = this.props;
-		// const { formData } = this.state;
 		return (
 			<TabPane tabId="contact">
 				<Card>
@@ -43,66 +16,38 @@ class ContactFormPane extends React.Component {
 						<i className="fas fa-envelope" /> Contact Me
 					</CardHeader>
 					<CardBody className="card-body">
-						<AvForm
-							data-spec="contact-form-container"
-							// onValidSubmit={() => submitContactForm(formData)}
-						>
-							<p>
-								Have a suggestion about the site? Encountered a bug? Want to just
-								say hi or give me a hug? Click th ebutton below to open an issue on
-								GitHub with your message.
-							</p>
-							<div className="container-github-button">
-								<div className="center-github-button">
-									<button
-										type="submit"
-										className="github-issue-link-button"
-										onClick={this.submitForm}
-									>
-										Go to Github
-									</button>
-								</div>
-							</div>
-							<br />
-							<p>
-								You can also visit the tracker{' '}
-								<a
-									target="_blank"
-									rel="noopener noreferrer"
-									href="http://tblrthreadtracker.tumblr.com"
-								>
-									support blog
-								</a>
-								to see if your question has recenly been answered.
-							</p>
-							{/* <FormGroup row>
-								<Col md="3">
-									<Label htmlFor="textarea-input">Message</Label>
-								</Col>
-								<Col xs="12" md="9">
-									<Input
-										type="textarea"
-										name="Message"
-										id="message-textarea"
-										rows="9"
-										onChange={this.handleInputChange}
-										placeholder="Enter your message here..."
-									/>
-								</Col>
-							</FormGroup>
-							<Row>
-								<Col className="text-right">
-									<Button type="submit" onClick={this.submitForm} color="primary">
-										Submit
-									</Button>
-								</Col>
-							</Row> */}
-						</AvForm>
+						<p>
+							Have a suggestion about the site? Encountered a bug? Want to just say hi
+							or give me a hug? Click th ebutton below to open an issue on GitHub with
+							your message.
+						</p>
+						<div className="text-center">
+							<a
+								className="btn btn-primary"
+								target="_blank"
+								rel="noopener noreferrer"
+								href="https://github.com/blackjackkent/RPThreadTrackerV3.FrontEnd/issues/new"
+							>
+								Go to Github
+							</a>
+						</div>
+						<br />
+						<p>
+							You can also visit the tracker{' '}
+							<a
+								target="_blank"
+								rel="noopener noreferrer"
+								href="http://tblrthreadtracker.tumblr.com"
+							>
+								support blog&nbsp;
+							</a>
+							to see if your question has recenly been answered.
+						</p>
 					</CardBody>
 				</Card>
 			</TabPane>
 		);
 	}
 }
-// ContactFormPane.propTypes = propTypes;
+
 export default ContactFormPane;
--- src/display/views/help/components/__tests__/ContactFormPane.spec.js
+++ src/display/views/help/components/__tests__/ContactFormPane.spec.js
@@ -1,7 +1,6 @@
 // #region imports
 import React from 'react';
 import { shallow } from 'enzyme';
-import { getSpecWrapper } from '~/testhelpers/helpers.unit';
 import ContactFormPane from '../ContactFormPane';
 // #endregion imports
 
@@ -10,53 +9,11 @@ const createTestProps = (propOverrides) => ({
 	...propOverrides
 });
 
-// The below test was for the old version when it was getting redirected as an email, now its getting redirected to github issues page as wanted.
-// Rest works fine.
-
 describe('rendering', () => {
-	// describe('snapshots', () => {
-	// 	it('should render valid snapshot', () => {
-	// 		const element = shallow(<ContactFormPane {...createTestProps()} />);
-	// 		expect(element).toMatchSnapshot();
-	// 	});
-	// });
-});
-
-describe('behavior', () => {
-	// describe('submitContactForm', () => {
-	// 	it('should be called when form is submitted', () => {
-	// 		const submitContactForm = jest.fn();
-	// 		const props = createTestProps({
-	// 			submitContactForm
-	// 		});
-	// 		const element = shallow(<ContactFormPane {...props} />);
-	// 		const form = getSpecWrapper(element, 'contact-form-container');
-	// 		form.props().onValidSubmit();
-	// 		expect(submitContactForm).toHaveBeenCalledTimes(1);
-	// 	});
-	// });
-	describe('handleInputChange', () => {
-		it('should handle text field update', () => {
-			const event = {
-				target: {
-					type: 'text',
-					name: 'message',
-					value: 'my-message'
-				}
-			};
-			const submitContactForm = jest.fn();
-			const props = createTestProps({
-				submitContactForm
-			});
-			const element = shallow(<ContactFormPane {...props} />);
-			element.instance().handleInputChange(event);
-			element.update();
-			const form = getSpecWrapper(element, 'contact-form-container');
-			form.props().onValidSubmit();
-			// expect(submitContactForm).toHaveBeenCalledTimes(1); // This test was failing due to changing the route to redirect o github.
-			// expect(submitContactForm).toHaveBeenLastCalledWith({
-			// 	message: 'my-message'
-			// });
+	describe('snapshots', () => {
+		it('should render valid snapshot', () => {
+			const element = shallow(<ContactFormPane {...createTestProps()} />);
+			expect(element).toMatchSnapshot();
 		});
 	});
 });
