e6d1299dbc8db8a8630ace2803a2604cb1424362
Rosalind W <blackjacksoftwaredev@gmail.com>
2021-01-11 03:55:21 -0600

    Adding useFormReducer

--- src/display/views/login/Login.js
+++ src/display/views/login/Login.js
@@ -1,8 +1,9 @@
 // #region imports
-import React, { useReducer } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Redirect } from 'react-router-dom';
 import { useLoginMutation } from '~/infrastructure/hooks/mutations';
+import useFormReducer from '~/infrastructure/hooks/useFormReducer';
 import cache from '~/infrastructure/cache';
 import cacheKeys from '~/infrastructure/constants/cacheKeys';
 import LoginForm from './components/LoginForm';
@@ -12,38 +13,13 @@ const propTypes = {
 	history: PropTypes.shape({ push: PropTypes.func }).isRequired
 };
 
-const initialState = {
-	username: '',
-	password: ''
-};
-const reducer = (state, action) => {
-	switch (action.type) {
-		case 'username':
-			return {
-				...state,
-				username: action.data
-			};
-		case 'password':
-			return {
-				...state,
-				password: action.data
-			};
-		default:
-			return state;
-	}
-};
-
 function Login() {
-	const [state, dispatch] = useReducer(reducer, initialState);
+	const [state, onInputChange] = useFormReducer();
 	const { mutate, reset, isLoading, isError, isSuccess, data, error } = useLoginMutation();
 
 	const onSubmit = () => {
 		reset();
 		mutate({ username: state.username, password: state.password });
-	};
-
-	const onInputChange = (event) => {
-		dispatch({ type: event.target.name, data: event.target.value });
 	};
 
 	if (isSuccess) {
--- src/infrastructure/hooks/useFormReducer.js
+++ src/infrastructure/hooks/useFormReducer.js
@@ -1,3 +1,23 @@
 import { useReducer } from 'react';
 
-const useFormReducer = (reducer, initialState) => {};
+const reducer = (state, action) => {
+	if (!action) {
+		return state;
+	}
+	return {
+		...state,
+		[action.type]: action.data
+	};
+};
+const useFormReducer = () => {
+	const [state, dispatch] = useReducer(reducer, {});
+	const onInputChange = (e) => {
+		const action = {
+			type: e.target.name,
+			data: e.target.value
+		};
+		dispatch(action);
+	};
+	return [state, onInputChange, dispatch];
+};
+export default useFormReducer;
