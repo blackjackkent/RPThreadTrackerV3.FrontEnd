6331f77f693d19a8c8af4ade2766112cd8dc70b7
Rosalind W <blackjacksoftwaredev@gmail.com>
2021-03-05 20:43:08 -0600

    Cleaning up sidebar code

--- src/display/shared/sidebar/Sidebar.js
+++ src/display/shared/sidebar/Sidebar.js
@@ -1,54 +1,36 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { Nav, NavItem } from 'reactstrap';
-import classNames from 'classnames';
 import nav from './_nav';
 import Style from './_styles';
 
-class Sidebar extends Component {
-	render() {
-		function wrapper(item) {
-			return React.createElement(item.wrapper.element, item.wrapper.attributes, item.name);
-		}
-		const title = (titleData, key) => {
-			const classes = classNames('nav-title', titleData.class);
+const Sidebar = () => {
+	const { items } = nav;
+	const navList = items.map((item) => {
+		if (item.title) {
 			return (
-				<li key={key} className={classes}>
-					{wrapper(titleData)}{' '}
+				<li key={item.name} className="nav-title">
+					<span>{item.name}</span>
 				</li>
 			);
-		};
-		const navItem = (item, key) => {
-			const classes = classNames('nav-link', item.class);
-			return (
-				<NavItem key={key}>
-					<NavLink to={item.url} className={classes} activeClassName="active">
-						<i className={item.icon} />
-						{item.name}
-					</NavLink>
-				</NavItem>
-			);
-		};
-		const navLink = (item, idx) => {
-			if (item.title) {
-				return title(item, idx);
-			}
-			return navItem(item, idx);
-		};
-
-		// nav list
-		// eslint-disable-next-line no-use-before-define
-		const navList = (items) => items.map((item, index) => navLink(item, index));
-
-		// sidebar-nav root
+		}
 		return (
-			<Style className="sidebar">
-				<nav className="sidebar-nav">
-					<Nav>{navList(nav.items)}</Nav>
-				</nav>
-			</Style>
+			<NavItem key={item.name}>
+				<NavLink to={item.url} className="nav-link" activeClassName="active">
+					<i className={item.icon} />
+					{item.name}
+				</NavLink>
+			</NavItem>
 		);
-	}
-}
+	});
+
+	return (
+		<Style className="sidebar">
+			<nav className="sidebar-nav">
+				<Nav>{navList}</Nav>
+			</nav>
+		</Style>
+	);
+};
 
 export default Sidebar;
