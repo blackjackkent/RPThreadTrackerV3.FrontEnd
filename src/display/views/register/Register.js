f528d36c17a2821ee4f1626024ae82f8e11fb1b8
Rosalind W <blackjacksoftwaredev@gmail.com>
2021-03-10 17:13:57 -0600

    Register hook and login fixes

--- src/display/views/register/Register.js
+++ src/display/views/register/Register.js
@@ -1,129 +1,96 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
+import React from 'react';
+import { Link, Redirect } from 'react-router-dom';
 import { CardBody, Button, Row, Col } from 'reactstrap';
-import { connect } from 'react-redux';
 import { AvForm } from 'availity-reactstrap-validation';
-import * as actions from '../../../infrastructure/actions';
 import RegisterForm from '../../forms/register/RegisterForm';
 import TooltipForm from '../../forms/TooltipForm';
 import Card from '../../shared/styled/Card';
 import LoadingIndicator from '../../shared/loading/LoadingIndicator';
+import { useLoginMutation, useRegisterMutation } from '~/infrastructure/hooks/mutations';
+import { useFormReducer } from '~/infrastructure/hooks';
 
-const propTypes = {
-	submitUserRegistration: PropTypes.func.isRequired,
-	registrationLoading: PropTypes.bool.isRequired,
-	loginLoading: PropTypes.bool.isRequired,
-	registrationErrors: PropTypes.arrayOf(PropTypes.string)
-};
-const defaultProps = {
-	registrationErrors: []
-};
-const mapStateToProps = (state) => {
-	const { loading, errors } = state;
-	return {
-		registrationLoading: loading.registrationLoading,
-		loginLoading: loading.loginLoading,
-		registrationErrors: errors.registrationErrors
+const Register = () => {
+	const [formData, onInputChange] = useFormReducer();
+	const {
+		submitUserRegistration,
+		reset,
+		isLoading: isUserRegistrationLoading,
+		isError: isUserRegistrationError,
+		isSuccess: isUserRegistrationSuccess,
+		error: registrationError
+	} = useRegisterMutation();
+	const {
+		submitLogin,
+		isLoading: isLoginLoading,
+		isSuccess: isLoginSuccess
+	} = useLoginMutation();
+
+	const handleRegistrationSubmit = () => {
+		reset();
+		submitUserRegistration(formData).then(() => {
+			submitLogin({ username: formData.username, password: formData.password }).then();
+		});
 	};
-};
 
-class Register extends Component {
-	constructor(props) {
-		super(props);
-		this.handleRegistrationSubmit = this.handleRegistrationSubmit.bind(this);
-		this.handleInputChange = this.handleInputChange.bind(this);
-		this.state = {
-			registerRequest: {}
-		};
-	}
-
-	handleRegistrationSubmit() {
-		const { submitUserRegistration } = this.props;
-		const { registerRequest } = this.state;
-		submitUserRegistration(registerRequest);
-	}
-
-	handleInputChange(event) {
-		const { target } = event;
-		const { name, value } = target;
-		this.setState((prevState) => ({
-			registerRequest: Object.assign({}, prevState.registerRequest, {
-				[name]: value
-			})
-		}));
-	}
-
-	render() {
-		const { registrationLoading, loginLoading, registrationErrors } = this.props;
-		const displayLoadingIndicator = registrationLoading || loginLoading;
-		let loading = <span />;
-		if (displayLoadingIndicator) {
-			loading = (
-				<LoadingIndicator
-					style={{
-						width: 50,
-						height: 50,
-						position: 'absolute',
-						top: 0,
-						right: 0
-					}}
-				/>
-			);
-		}
-		let error = <span />;
-		if (registrationErrors.length) {
-			const errorStrings = registrationErrors.map((e) => (
-				<span key={e}>
-					{e}
-					<br />
-				</span>
-			));
-			error = (
-				<div className="has-danger">
-					<p data-spec="register-server-error" className="form-control-feedback">
-						{errorStrings}
-					</p>
-				</div>
-			);
-		}
+	if (isUserRegistrationSuccess && isLoginSuccess) {
 		return (
-			<Card className="mx-4">
-				<CardBody className="p-4">
-					{loading}
-					<AvForm
-						data-spec="registration-form-container"
-						onValidSubmit={this.handleRegistrationSubmit}
-					>
-						<h1>Register</h1>
-						<p className="text-muted">Create your RPThreadTracker account</p>
-						{error}
-						<TooltipForm
-							Renderable={RegisterForm}
-							handleInputChange={this.handleInputChange}
-						/>
-						<Row>
-							<Col xs="6">
-								<Button color="primary" className="px-4">
-									Create Account
-								</Button>
-							</Col>
-							<Col xs="6" className="text-right text-muted">
-								Already have an account?{' '}
-								<Link href="/login" to="/login">
-									Login
-								</Link>
-							</Col>
-						</Row>
-					</AvForm>
-				</CardBody>
-			</Card>
+			<div>
+				<Redirect to="/dashboard" />
+			</div>
 		);
 	}
-}
 
-Register.propTypes = propTypes;
-Register.defaultProps = defaultProps;
-export default connect(mapStateToProps, {
-	submitUserRegistration: actions.submitUserRegistration
-})(Register);
+	const shouldDisplayLoadingIndicator = isUserRegistrationLoading || isLoginLoading;
+	console.log(registrationError?.response);
+	return (
+		<Card className="mx-4">
+			<CardBody className="p-4">
+				{shouldDisplayLoadingIndicator && (
+					<LoadingIndicator
+						style={{
+							width: 50,
+							height: 50,
+							position: 'absolute',
+							top: 0,
+							right: 0
+						}}
+					/>
+				)}
+				<AvForm
+					data-spec="registration-form-container"
+					onValidSubmit={handleRegistrationSubmit}
+				>
+					<h1>Register</h1>
+					<p className="text-muted">Create your RPThreadTracker account</p>
+					{isUserRegistrationError && (
+						<div className="has-danger">
+							<p data-spec="register-server-error" className="form-control-feedback">
+								{registrationError?.response?.data?.map((e) => (
+									<span key={e}>
+										{e}
+										<br />
+									</span>
+								))}
+							</p>
+						</div>
+					)}
+					<TooltipForm Renderable={RegisterForm} handleInputChange={onInputChange} />
+					<Row>
+						<Col xs="6">
+							<Button color="primary" className="px-4">
+								Create Account
+							</Button>
+						</Col>
+						<Col xs="6" className="text-right text-muted">
+							Already have an account?{' '}
+							<Link href="/login" to="/login">
+								Login
+							</Link>
+						</Col>
+					</Row>
+				</AvForm>
+			</CardBody>
+		</Card>
+	);
+};
+export default Register;
